00a3d20c9fc8a5a88d9e1ab2127200e46804ea05
orizhiy <sklif.85@gmail.com>
2017-04-14 21:54:15 +0300

    day09

---
+++ JS_day_09/ex02/todo.js
@@ -0,0 +1,24 @@
+
+function prompt_task()
+{
+    // console.log("Clicked the button...");
+    var text = prompt("Fill in a new TO DO in your list: ", "");
+    if (text)
+        add_task(text);
+    // else
+        // console.log("Canceled...");
+
+    function add_task(text) {
+        var new_elem = document.createElement("Li");
+        // new_elem.id = new_elem_id;
+        new_elem.appendChild(document.createTextNode(text));
+        var list = document.getElementById("ft_list");
+        list.insertBefore(new_elem, list.childNodes[0]);
+
+        new_elem.addEventListener('click', function (event) {
+            var q = confirm("Are you shure?");
+            if (q)
+                event.target.remove();
+        })
+    }
+}
