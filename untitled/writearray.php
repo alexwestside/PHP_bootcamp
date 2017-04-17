e4542c2f9927e718e795627fc716a201eceb7903
orizhiy <sklif.85@gmail.com>
2017-04-17 12:54:00 +0300

    rush_01

--- untitled/writearray.php
+++
@@ -1,6 +0,0 @@
-<?php
-file_put_contents("mysql.cfg", serialize(array( "db_host" => "localhost",
-                    "db_user" => "root",
-                    "db_pass" => "goldfish12",
-                    "db_name" => "rush01")));
- ?>
